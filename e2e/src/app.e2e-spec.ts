f51dc7dba0b4c092e5dd3eb762e1e0e8f9b4b1e5
ajitsinghkaler <ajitsinghkaler0@gmail.com>
2019-11-10 13:51:14 +0530

    Changing to npm component started

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('karza-assign app is running!');
+    expect(page.getTitleText()).toEqual('angular-header app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'karza-assign'`, () => {
+  it(`should have as title 'angular-header'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('karza-assign');
+    expect(app.title).toEqual('angular-header');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('karza-assign app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-header app is running!');
   });
 });
